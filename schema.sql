85fde897f313b3ec4631045abc448a823f1487ff
jobatabs <2370315+jobatabs@users.noreply.github.com>
2023-03-04 19:54:50 +0200

    Update cafes table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
+CREATE TABLE cafes (id SERIAL PRIMARY KEY, name TEXT, description TEXT, added TIMESTAMP, updated TIMESTAMP, added_by INTEGER REFERENCES users, visible BOOLEAN);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, role INTEGER);

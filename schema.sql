7c1c170f05b3cb704bc684ecd5cea122b5306e80
jobatabs <2370315+jobatabs@users.noreply.github.com>
2023-03-04 21:20:11 +0200

    Require usernames to be unique

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE cafes (id SERIAL PRIMARY KEY, name TEXT, description TEXT, added TIMESTAMP, updated TIMESTAMP, added_by INTEGER REFERENCES users, visible BOOLEAN);
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, role INTEGER);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, role INTEGER);
 CREATE TABLE tags (id SERIAL PRIMARY KEY, name TEXT);
 CREATE TABLE tagmap (id SERIAL PRIMARY KEY, cafe_id INTEGER REFERENCES cafes, tag_id INTEGER REFERENCES tags);
 CREATE TABLE reviews (id SERIAL PRIMARY KEY, cafe_id INTEGER REFERENCES cafes, author INTEGER REFERENCES users, review TEXT, added TIMESTAMP, visible BOOLEAN);
